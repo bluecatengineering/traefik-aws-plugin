4b0b9102191cc9fcdf382010cc2e283f5af9458c
ffauteux@bluecatnetworks.com <ffauteux@bluecatnetworks.com>
2023-11-22 09:04:20 -0500

    EDG-13557: Move aws-sink to traefik-aws-plugin

---
+++ ecs/ecs.go
@@ -0,0 +1,65 @@
+package ecs
+
+import (
+	"encoding/json"
+	"github.com/bluecatengineering/traefik-aws-plugin/log"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+const onErrorRotationInterval = 10 * time.Second
+
+type Credentials struct {
+	AccessKeyId     string    `json:"AccessKeyId"`
+	AccessSecretKey string    `json:"SecretAccessKey"`
+	SecurityToken   string    `json:"Token"`
+	RoleArn         string    `json:"RoleArn"`
+	Expiration      time.Time `json:"Expiration"`
+}
+
+func GetCredentials() *Credentials {
+	creds := &Credentials{}
+	go getCredentials(creds)
+	return creds
+}
+
+// https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-iam-roles.html
+func getCredentials(creds *Credentials) {
+	client := &http.Client{}
+	credsUri := "http://169.254.170.2" + os.Getenv("AWS_CONTAINER_CREDENTIALS_RELATIVE_URI")
+	for {
+		err := refreshCredentials(creds, client, credsUri)
+		if err != nil {
+			log.Error(err.Error())
+			onErrorTimer := time.NewTimer(onErrorRotationInterval)
+			<-onErrorTimer.C
+		} else {
+			// Renew when half the lifetime is reached
+			duration := time.Until(creds.Expiration) / 2
+			renewalTimer := time.NewTimer(duration)
+			<-renewalTimer.C
+		}
+	}
+}
+
+func refreshCredentials(creds *Credentials, client *http.Client, credsUri string) error {
+	req, err := http.NewRequest("GET", credsUri, nil)
+	if err != nil {
+		return err
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(body, creds)
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- signer/signer_test.go
+++ signer/signer_test.go
@@ -3,33 +3,32 @@ package signer
 import (
 	"bytes"
 	"fmt"
-	"github.com/yoeluk/aws-sink/aws"
+	"github.com/bluecatengineering/traefik-aws-plugin/ecs"
 	"net/http"
 	"testing"
 )
 
 func TestCanonRequestV4(t *testing.T) {
 	testCases := []struct {
-		sig           *CanonRequest
-		expectedSig   string
-		expectedAuthZ string
-		name          string
+		cr                 *CanonRequest
+		expectedSig        string
+		expectedAuthHeader string
+		name               string
 	}{
 		{
-			name:          "get object request",
-			expectedSig:   "f0e8bdb87c964420e857bd35b5d6ed310bd44f0170aba48dd91039c6036bdb41",
-			expectedAuthZ: "AWS4-HMAC-SHA256 Credential=AKIAIOSFODNN7EXAMPLE/20130524/us-east-1/s3/aws4_request,SignedHeaders=host;range;x-amz-content-sha256;x-amz-date,Signature=f0e8bdb87c964420e857bd35b5d6ed310bd44f0170aba48dd91039c6036bdb41",
-			sig: &CanonRequest{
-				Creds: &aws.Credentials{
+			name:               "get object request",
+			expectedSig:        "f0e8bdb87c964420e857bd35b5d6ed310bd44f0170aba48dd91039c6036bdb41",
+			expectedAuthHeader: "AWS4-HMAC-SHA256 Credential=AKIAIOSFODNN7EXAMPLE/20130524/us-east-1/s3/aws4_request,SignedHeaders=host;range;x-amz-content-sha256;x-amz-date,Signature=f0e8bdb87c964420e857bd35b5d6ed310bd44f0170aba48dd91039c6036bdb41",
+			cr: &CanonRequest{
+				Creds: &ecs.Credentials{
 					AccessSecretKey: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
 					AccessKeyId:     "AKIAIOSFODNN7EXAMPLE",
 				},
-				httpVerb:       "GET",
-				date:           "20130524T000000Z",
-				Region:         "us-east-1",
-				Service:        "s3",
-				resource:       "/test.txt",
-				VersionRequest: "aws4_request",
+				httpMethod: "GET",
+				date:       "20130524T000000Z",
+				Region:     "us-east-1",
+				Service:    "s3",
+				canonUri:   "/test.txt",
 				amzHeaders: map[string]string{
 					"host":                 "examplebucket.s3.amazonaws.com",
 					"range":                "bytes=0-9",
@@ -39,20 +38,19 @@ func TestCanonRequestV4(t *testing.T) {
 			},
 		},
 		{
-			name:          "put object request",
-			expectedSig:   "98ad721746da40c64f1a55b78f14c238d841ea1380cd77a1b5971af0ece108bd",
-			expectedAuthZ: "AWS4-HMAC-SHA256 Credential=AKIAIOSFODNN7EXAMPLE/20130524/us-east-1/s3/aws4_request,SignedHeaders=date;host;x-amz-content-sha256;x-amz-date;x-amz-storage-class,Signature=98ad721746da40c64f1a55b78f14c238d841ea1380cd77a1b5971af0ece108bd",
-			sig: &CanonRequest{
-				Creds: &aws.Credentials{
+			name:               "put object request",
+			expectedSig:        "98ad721746da40c64f1a55b78f14c238d841ea1380cd77a1b5971af0ece108bd",
+			expectedAuthHeader: "AWS4-HMAC-SHA256 Credential=AKIAIOSFODNN7EXAMPLE/20130524/us-east-1/s3/aws4_request,SignedHeaders=date;host;x-amz-content-sha256;x-amz-date;x-amz-storage-class,Signature=98ad721746da40c64f1a55b78f14c238d841ea1380cd77a1b5971af0ece108bd",
+			cr: &CanonRequest{
+				Creds: &ecs.Credentials{
 					AccessSecretKey: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
 					AccessKeyId:     "AKIAIOSFODNN7EXAMPLE",
 				},
-				httpVerb:       "PUT",
-				date:           "Fri, 24 May 2013 00:00:00 GMT",
-				Region:         "us-east-1",
-				Service:        "s3",
-				resource:       "/test%24file.text",
-				VersionRequest: "aws4_request",
+				httpMethod: "PUT",
+				date:       "Fri, 24 May 2013 00:00:00 GMT",
+				Region:     "us-east-1",
+				Service:    "s3",
+				canonUri:   "/test%24file.text",
 				amzHeaders: map[string]string{
 					"host":                 "examplebucket.s3.amazonaws.com",
 					"date":                 "Fri, 24 May 2013 00:00:00 GMT",
@@ -65,26 +63,25 @@ func TestCanonRequestV4(t *testing.T) {
 	}
 
 	for _, tt := range testCases {
-		signature := tt.sig.SignatureV4()
-		authz := tt.sig.AuthHeader()
+		signature := tt.cr.SignatureV4()
+		authHeader := tt.cr.AuthHeader()
 		if signature != tt.expectedSig {
-			t.Errorf("found an error while testing %s: the signature didn't match the expected string\nexpected: %s\nfound: %s\nStringToSign:\n%s\nRequestString: \n%s\n", tt.name, tt.expectedSig, signature, tt.sig.StringToSignV4(), tt.sig.RequestString())
+			t.Errorf("found an error while testing %s: the signature didn't match the expected string\nexpected: %s\nfound: %s\nStringToSign:\n%s\nRequestString: \n%s\n", tt.name, tt.expectedSig, signature, tt.cr.StringToSignV4(), tt.cr.RequestString())
 		}
-		if authz != tt.expectedAuthZ {
-			t.Errorf("found unexpected authorization header; expected:\n%s,\nfound:\n%s", tt.expectedAuthZ, authz)
+		if authHeader != tt.expectedAuthHeader {
+			t.Errorf("found unexpected authorization header; expected:\n%s,\nfound:\n%s", tt.expectedAuthHeader, authHeader)
 		}
 	}
 }
 
 func TestSignerV4(t *testing.T) {
 	crTemplate := &CanonRequest{
-		Creds: &aws.Credentials{
+		Creds: &ecs.Credentials{
 			AccessSecretKey: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
 			AccessKeyId:     "AKIAIOSFODNN7EXAMPLE",
 		},
-		Region:         "us-east-1",
-		Service:        "s3",
-		VersionRequest: "aws4_request",
+		Region:  "us-east-1",
+		Service: "s3",
 	}
 
 	r1, _ := http.NewRequest("PUT", "https://examplebucket.s3.amazonaws.com/text/test/file.text", bytes.NewReader([]byte("")))
@@ -104,7 +101,7 @@ func TestSignerV4(t *testing.T) {
 	for _, tt := range testCases {
 		r1.Header.Set("Content-Type", "application/json")
 		r1.Header.Set("Host", r1.URL.Host)
-		cr := Signer(tt.request, make([]byte, 0), *crTemplate)
+		cr := CreateCanonRequest(tt.request, make([]byte, 0), *crTemplate)
 		fmt.Printf("%v\n", cr.AuthHeader())
 		fmt.Printf("%v\n", cr.StringToSignV4())
 		fmt.Printf("%v\n", cr.RequestString())
